2c4177f2e0c826106a3257e84c63d63b4aa74e03
ski0090 <45457350+ski0090@users.noreply.github.com>
2020-07-17 14:15:44 +0900

    start class

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <div>Hello!</div>
-  ;
+class App extends React.Component{
+  state={
+    count:0
+  };
+  add=()=>{
+    this.setState({count:this.state.count +1});
+  };
+  minus=()=>{
+    this.setState({count:this.state.count -1});
+  };
+  render (){
+    return (
+    <div>
+      <h1>
+         안녕 난 {this.state.count}만큼잘생긴 평강이야!
+      </h1>
+      <button onClick={this.add}>+</button> 
+      <button onClick={this.minus}>-</button>
+    </div>
+    )
+  }
 }
-
 export default App;
